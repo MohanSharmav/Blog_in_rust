91dfad1f3cf94fe3e4eec510b0b7661b2f40cdd1
MohanSharmav <catchmohansharma@gmail.com>
2023-04-27 14:39:10 +0900

    Model files done

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+mod model;
+mod controller;
+use std::path::Path;
+use sqlx::postgres::PgPoolOptions;
+use actix_files::NamedFile;
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Result, web};
+use actix_web::http::StatusCode;
+use tokio::select;
+use warp::reply::with_status;
+
+use model::database::selecting;
+use warp::{Rejection, Reply};
+
+async fn index(req: HttpRequest)-> Result<NamedFile>{
+     let path= Path::new("src/one.html");
+     Ok(NamedFile::open(path)?)
+}
+
+
+#[tokio::main]
+async fn main() -> Result<()>{
+
+
+     //database
+selecting().await.expect("TODO: panic message");
+
+
+     HttpServer::new(|| {
+          App::new()
+              .service(web::resource("/").to(index))
+              .service(web::resource("/hi").to(index))
+     })
+         .bind("127.0.0.1:8080")?
+         .run().await.expect("TODO: panic message");
+     Ok(())
+}
